83460523fc3da17f70a9066ea62a281d5f2488b4
LucasTamai <lucas.tamai@usp.br>
2020-01-07 18:35:45 -0300

    LoginValidation

--- LucasTamaiOnboard/LoginScreen.tsx
+++ LucasTamaiOnboard/LoginScreen.tsx
@@ -10,6 +10,23 @@ export default class Login extends Component{
         password: ''
      }
 
+     handleEmail = (text:String) => {
+        this.setState({ email: text })
+     }
+
+     handlePassword = (text) => {
+        this.setState({ password: text })
+     }
+
+    validateLogin = (email:String, password:String) => {
+        const expressionEmail = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        const expressionPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
+        if(expressionEmail.test(email.toLowerCase()) && expressionPassword.test(password.toLowerCase())){
+            return(alert("Login efetuado"))
+        }
+        else return(alert("Erro ao logar"))
+    }
+    
     render(){
         return(
             <View style={Styles.container}>
@@ -21,6 +38,7 @@ export default class Login extends Component{
                     placeholder = "Email"
                     placeholderTextColor = "#9a73ef"
                     autoCapitalize = "none"
+                    onChangeText = {this.handleEmail}
                 />
 
                 <Text style={Styles.inputHeader}>Senha</Text>
@@ -29,10 +47,13 @@ export default class Login extends Component{
                     placeholder = "Password"
                     placeholderTextColor = "#9a73ef"
                     autoCapitalize = "none"
+                    onChangeText = {this.handlePassword}
                 />
 
                 <TouchableOpacity 
-                    style = {Styles.submitButton}>
+                    style = {Styles.submitButton}
+                    onPress = {() => this.validateLogin(this.state.email, this.state.password)}
+                    >
                     <Text style={Styles.submitButtonText}>Entrar</Text>
                 </TouchableOpacity>
 
