17ffb4318adc12fc60a741e217965cf6322a85cc
LucasTamai <lucas.tamai@usp.br>
2020-01-08 10:42:45 -0300

    loginScreen

--- LucasTamaiOnboard/App.tsx
+++ LucasTamaiOnboard/App.tsx
@@ -7,6 +7,7 @@
  */
 import React from 'react';
 import { Component } from 'react';
+import LoginScreen from "./LoginScreen";
 import {
   SafeAreaView,
   StyleSheet,
@@ -27,63 +28,10 @@ import {
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>Hello, world!</Text>
-        </View>
-        </ScrollView>
-      </SafeAreaView>
+      <LoginScreen/>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
