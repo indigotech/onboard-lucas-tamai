f67a29bcc5e40a65b6e2a8ca82e41f025dfa0d51
LucasTamai <lucas.tamai@usp.br>
2020-01-13 12:00:48 -0300

    fix env and loading

--- LucasTamaiOnboard/LoginValidator.tsx
+++ LucasTamaiOnboard/LoginValidator.tsx
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 import { Navigation } from "react-native-navigation";
 import UserListScreen from "./UserListScreen";
 import {client} from "./ApolloClient";
+import { Alert, AsyncStorage } from "react-native";
 
 export function validateLogin(email:string, password:string) {
     const expressionEmail = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
@@ -15,8 +16,9 @@ export function validateLogin(email:string, password:string) {
     }
 }
 
-export function doLogin(email:string,password:string){
-    client.mutate({ 
+export async function doLogin(email:string,password:string): Promise<string>{ 
+    
+    const result = await client.mutate({ 
         mutation: gql`
         mutation Autenticate{
             Login(data: {
@@ -26,17 +28,18 @@ export function doLogin(email:string,password:string){
             token
             }
         }  
-    `}).then(result => {
-        storeToken(result)
-        changePage()
-    }).catch(error => alert(error))
-}
-
-export function storeToken(result:any){
-    const Token = JSON.stringify(result.data.Login.token);
+    `})
     
+    return(result.data.Login.token)
 }
+    
+export const storeToken = async (key:string, token:string) => {
+    try{
+        await AsyncStorage.setItem(key,token)        
+    }
+    catch(error){}
 
-function changePage(){
+}
+export function changePage(){
     Navigation.setRoot({root: {component: {name: "UserListScreen"} } })
 }
