5f2a5812015a8119ef5a7d9c245840143e2ee40b
LucasTamai <lucas.tamai@usp.br>
2020-01-13 12:00:48 -0300

    fix env and loading

--- LucasTamaiOnboard/index.js
+++ LucasTamaiOnboard/index.js
@@ -2,8 +2,6 @@ import { registerScreens } from './screens'
 import { Navigation } from 'react-native-navigation'
 import React from 'react'
 
-
-
 registerScreens()
 
 Navigation.events().registerAppLaunchedListener(() => {
