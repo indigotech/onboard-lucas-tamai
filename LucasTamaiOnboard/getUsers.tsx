cdddd830159aba2c09bbf94216fa48e24e8683f2
LucasTamai <lucas.tamai@usp.br>
2020-01-14 15:36:02 -0300

    Separacao Login Validator

---
+++ LucasTamaiOnboard/getUsers.tsx
@@ -0,0 +1,35 @@
+import { client } from "./ApolloClient";
+import { gql } from "@apollo/client";
+import { Alert, AsyncStorage } from "react-native";
+
+export async function getUsers(offset:number){ 
+
+    const token = await AsyncStorage.getItem("token")
+
+    const query =  gql`
+        query getUsers{
+            Users(offset:${offset}){
+            nodes{
+                id,
+                name,
+                email,   
+            }
+            } 
+        }  
+        `
+
+    if(token){
+        const context = setHeader(token)
+        const result = await client.query({query:query, context:context})
+        return(result.data.Users.nodes)
+
+    }
+}
+    
+function setHeader(token:string){
+    return {
+        headers: {
+          authorization: `${token}`,
+        }
+      }
+}
